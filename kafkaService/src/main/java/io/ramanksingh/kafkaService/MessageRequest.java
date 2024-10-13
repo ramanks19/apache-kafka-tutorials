1030903daf4123770534d8dc0382d4a706300198
Raman Kumar Singh <75598310+ramanks19@users.noreply.github.com>
2024-10-13 13:36:08 +0530

    Update MessageRequest.java

--- kafkaService/src/main/java/io/ramanksingh/kafkaService/MessageRequest.java
+++ kafkaService/src/main/java/io/ramanksingh/kafkaService/MessageRequest.java
@@ -1,5 +1,8 @@
 package io.ramanksingh.kafkaService;
 
+/**
+ * A simple DTO, used to encapsulate the data sent in HTTP requests for publishing messages
+ * */
 public record MessageRequest(String message) {
 
 }
